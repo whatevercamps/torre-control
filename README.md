84a4b1ca182e71f017c47d3ee23c1e696eac71b7
David Bautista <dj.bautista10@uniandes.edu.co>
2021-02-20 08:14:58 -0500

    Readme created and other structure changes

---
+++ README.md
@@ -0,0 +1,25 @@
+# Torre-control 🛫
+
+Hi! I'm David,
+
+![Pic of a torre control](https://www.itmplatform.com/lib/uploads/59609188_m.jpg "Pic of a torre control")
+
+Taken from ITM Platform https://www.itmplatform.com/es/blog/la-pmo-como-una-torre-de-control/
+
+## Concept
+
+## Logbook
+
+# Nerds stuff
+
+## Third-part services
+
+## Arch
+
+## How to deploy
+
+## Next steps (TO-DO)
+
+Thanks for reading...
+
+David 🤓
--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+"use strict";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("dotenv").config();
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
